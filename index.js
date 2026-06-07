01e1262a8a7f20d50d985381a2d4f1d0ff5fea2b
iamatultiwwari <atult2386@gmail.com>
2026-06-07 13:06:46 +0530

    added server

---
+++ index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+dotenv.config();
+import express from "express";
+const app = express();
+
+const port = process.env.port;
+
+app.get("/",(req,res) => {
+    res.send("hello their")
+});
+
+
+app.listen(port,() => {
+    console.log(`the server is running on port${port}`)
+})
