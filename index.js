911823cc22dce239622986073975b195e1d99189
iamatultiwwari <atult2386@gmail.com>
2026-06-07 14:16:59 +0530

    added sum fn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 const app = express();
 
 const port = process.env.port;
+import sum from "./sum.js";
 
 app.get("/",(req,res) => {
     res.send("hello their")
@@ -20,3 +21,12 @@ app.get("/home", async (req,res)  => {
         msg:"i am root"
     });
 })
+
+
+app.get("/getsum/:a/:b", async (req, res) => {
+  const { a, b } = req.params;
+
+  res.json({
+    ans: sum(parseInt(a), parseInt(b))
+  });
+});
